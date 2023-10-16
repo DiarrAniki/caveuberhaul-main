90cb662a9095cf40decb1eb593bf4fd7ba06314d
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-15 19:59:35 -0500

    keybind to clear profiler times

--- src/main/java/diarr/caveuberhaul/mixin/MinecraftMixin.java
+++ src/main/java/diarr/caveuberhaul/mixin/MinecraftMixin.java
@@ -1,7 +1,9 @@
 package diarr.caveuberhaul.mixin;
 
+import diarr.caveuberhaul.CaveUberhaul;
 import diarr.caveuberhaul.Profiler;
 import net.minecraft.client.Minecraft;
+import org.lwjgl.input.Keyboard;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +18,10 @@ public class MinecraftMixin {
     private void checkProfileTimes(CallbackInfo ci){
         if (ticksRan % 100 == 0){
             Profiler.printTimesInRespectToID("Minecraft");
-            //Profiler.clearTimes();
+        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) && Keyboard.isKeyDown(Keyboard.KEY_BACK)){
+            CaveUberhaul.LOGGER.info("CLEARING TIMES");
+            Profiler.clearTimes();
         }
         Profiler.methodStart("Minecraft");
     }
