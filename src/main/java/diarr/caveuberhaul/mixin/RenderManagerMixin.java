dfc2ed18c55464dbc4f7e0527628c96d2328f947
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-16 13:09:27 -0500

    Cleaned Up Render ManagerMixin

--- src/main/java/diarr/caveuberhaul/mixin/RenderManagerMixin.java
+++ src/main/java/diarr/caveuberhaul/mixin/RenderManagerMixin.java
@@ -4,30 +4,25 @@ import diarr.caveuberhaul.blocks.EntityFallingStalagtite;
 import diarr.caveuberhaul.blocks.RenderFallingStalagtite;
 import net.minecraft.client.render.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.EntityRenderer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Iterator;
 import java.util.Map;
 
 @Mixin(value= EntityRenderDispatcher.class,remap = false)
 public class RenderManagerMixin {
+    @Final
     @Shadow
     private Map<Class<?>, EntityRenderer<?>> renderers;
-    @Inject(method = "<init>", at = @At("TAIL"),cancellable = false)
+    @Inject(method = "<init>", at = @At("TAIL"))
     private void RenderManager(CallbackInfo ci)
     {
-        renderers.put(EntityFallingStalagtite.class, new RenderFallingStalagtite());
-
-        Iterator var1 = this.renderers.values().iterator();
-
-        while(var1.hasNext()) {
-            EntityRenderer<?> renderer = (EntityRenderer)var1.next();
-            renderer.setRenderDispatcher((EntityRenderDispatcher) (Object)this);
-        }
-        //ci.cancel();
+        RenderFallingStalagtite fallingStalagtite = new RenderFallingStalagtite();
+        renderers.put(EntityFallingStalagtite.class, fallingStalagtite);
+        fallingStalagtite.setRenderDispatcher((EntityRenderDispatcher) (Object)this);
     }
 }
